f39fdc0e55c2ebc8cee3f34edd7a5b87ac934a2b
mohankumar-mg <mohankumargandhi09@gmail.com>
2024-10-04 11:13:23 +0530

    dropdown incorrect path rectified in navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Dropdown from "./DropDown";
+import Dropdown from "./Dropdown";
 import Phone from "../assets/phone.svg";
 import {MyAccount} from "../constants/DropdownLists";
 
